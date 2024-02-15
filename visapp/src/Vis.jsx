59353b8fc999360c16aa52e996b6e351b2475009
Dante Sangregorio <dantes@kth.se>
2024-02-15 14:02:52 +0100

    dsiplay 100 countreis

--- visapp/src/Vis.jsx
+++ visapp/src/Vis.jsx
@@ -7,9 +7,9 @@ const EmissionsPerCapitaChart = ({ countriesData }) => {
 
   useEffect(() => {
     drawChart();
-  }, [countriesData]);
+}, [countriesData]);
 
-  const drawChart = () => {
+const drawChart = () => {
     if (!countriesData.length) return; // Return if data is empty
 
     const margin = { top: 20, right: 30, bottom: 40, left: 50 };
@@ -20,48 +20,48 @@ const EmissionsPerCapitaChart = ({ countriesData }) => {
     const sortedData = countriesData.sort((a, b) => b['2021'] - a['2021']);
 
     // Use only the top 10 countries with the highest emissionsPerCapita values
-    const topCountriesData = sortedData.slice(0, 10);
+    const topCountriesData = sortedData.slice(0, 100);
 
     const svg = d3.select(chartRef.current)
-      .attr('width', width + margin.left + margin.right)
-      .attr('height', height + margin.top + margin.bottom)
-      .append('g')
-      .attr('transform', `translate(${margin.left},${margin.top})`);
+    .attr('width', width + margin.left + margin.right)
+    .attr('height', height + margin.top + margin.bottom)
+    .append('g')
+    .attr('transform', `translate(${margin.left},${margin.top})`);
 
     const x = d3.scaleBand()
-      .domain(topCountriesData.map(d => d.country))
-      .range([0, width])
-      .padding(0.1);
+    .domain(topCountriesData.map(d => d.country))
+    .range([0, width])
+    .padding(0.1);
 
     const y = d3.scaleLinear()
-      .domain([0, d3.max(topCountriesData, d => d['2021'])])
-      .nice()
-      .range([height, 0]);
+    .domain([0, d3.max(topCountriesData, d => d['2021'])])
+    .nice()
+    .range([height, 0]);
 
     svg.append('g')
-      .attr('class', 'x-axis')
-      .attr('transform', `translate(0,${height})`)
-      .call(d3.axisBottom(x))
-      .selectAll('text')
-      .style('text-anchor', 'end')
-      .attr('dx', '-.8em')
-      .attr('dy', '.15em')
-      .attr('transform', 'rotate(-45)');
+    .attr('class', 'x-axis')
+    .attr('transform', `translate(0,${height})`)
+    .call(d3.axisBottom(x))
+    .selectAll('text')
+    .style('text-anchor', 'end')
+    .attr('dx', '-.8em')
+    .attr('dy', '.15em')
+    .attr('transform', 'rotate(-45)');
 
     svg.append('g')
-      .attr('class', 'y-axis')
-      .call(d3.axisLeft(y).ticks(5));
+    .attr('class', 'y-axis')
+    .call(d3.axisLeft(y).ticks(5));
 
     svg.selectAll('.bar')
-      .data(topCountriesData)
-      .enter().append('rect')
-      .attr('class', 'bar')
-      .attr('x', d => x(d.country))
-      .attr('y', d => y(d['2021']))
-      .attr('width', x.bandwidth())
-      .attr('height', d => height - y(d['2021']))
-      .attr('fill', 'steelblue');
-  };
+    .data(topCountriesData)
+    .enter().append('rect')
+    .attr('class', 'bar')
+    .attr('x', d => x(d.country))
+    .attr('y', d => y(d['2021']))
+    .attr('width', x.bandwidth())
+    .attr('height', d => height - y(d['2021']))
+    .attr('fill', 'steelblue');
+};
 
   return <svg ref={chartRef}></svg>;
 };
