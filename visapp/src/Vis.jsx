039b6fee6134cdee8e403b03a4a5e92078bb4457
Dante Sangregorio <dantes@kth.se>
2024-02-20 11:08:38 +0100

    continents vissas som undeffiend :(

--- visapp/src/Vis.jsx
+++ visapp/src/Vis.jsx
@@ -27,6 +27,7 @@ function Vis(){
         electric: false
     });
     const [rightDisplay, setRightDisplay] = useState(0);
+    const [continentFilter, setContinentFilter] = useState('All');
 
     const svgRef = useRef();
 
@@ -71,16 +72,26 @@ function Vis(){
 
     useEffect(()=>{
         const svg = select(svgRef.current);
+        const filteredCountryData = continentFilter === 'All' ? countryData : countryData.filter(countryData => countryData.continent === continentFilter);
+
 
         const bar_window_size = {width: svgSize.width - Settings.border * 2, height: svgSize.height - Settings.border * 2}
-        const bar_width = bar_window_size.width / countryData.length;
+        const bar_width = bar_window_size.width / filteredCountryData.length;
         const y_scale = scaleLinear([0, Settings.y_max],[0, bar_window_size.height]);
         const reverse_y_scale = scaleLinear([0, Settings.y_max],[bar_window_size.height, 0]);
+        
+        filteredCountryData.forEach(country => {
+            console.log(`Country: ${country.country}, Continent: ${country.continent}`);
+        });
+        
+
+    
+
 
         const yAxis = axisRight(reverse_y_scale);
 
         // this one should be replaced with countries
-        const xAxis = axisBottom(scaleLinear([1, countryData.length + 1],[0, bar_window_size.width ]));
+        const xAxis = axisBottom(scaleLinear([1, data.length + 1],[0, bar_window_size.width ]));
 
         const gy = svg.selectAll(".y-axis").data([null]);
 
@@ -108,7 +119,7 @@ function Vis(){
             .attr("class", "x-axis")
         .merge(gx)
             .attr("transform", `translate(${Settings.border},${Settings.border + bar_window_size.height + 5})`)
-            .call(xAxis.ticks(svgSize.width > 600 ? countryData.length : countryData.length / 2))
+            .call(xAxis.ticks(svgSize.width > 600 ? filteredCountryData.length : filteredCountryData.length / 2))
             .call(g => g.select(".domain").remove())
             .selectAll(".tick")
             .each(function(d, i, nodes) {
@@ -126,35 +137,36 @@ function Vis(){
         // .attr("x", function(d, i) { return (bar_window_size.width / data.length) * i + Settings.border})
         // .attr("y", (d) => {return y_scale(Settings.y_max - d) + Settings.border });
 
-        svg.selectAll('.first').data(countryData).join(
+        svg.selectAll('.first').data(filteredCountryData).join(
             enter => enter.append('rect').attr('class', 'first'),
             update => update.attr('class', 'first'),
             exit => exit.remove()
-        ).attr('width', () => { return Math.max(0, (bar_window_size.width / countryData.length) * Settings.bar_size)})
+        ).attr('width', () => { return Math.max(0, (bar_window_size.width / filteredCountryData.length) * Settings.bar_size)})
         .attr('height', function(d) { return Math.max(0, y_scale(d['2022'])); })
-        .attr("x", function(d, i) { return (bar_window_size.width / countryData.length) * i + Settings.border})
+        .attr("x", function(d, i) { return (bar_window_size.width / filteredCountryData.length) * i + Settings.border})
         .attr("y", (d) => {return y_scale(Settings.y_max - d['2022']) + Settings.border })
         .on('click', (p_e,d) => {
             setSelectedCountry(d);
         });
         console.log(selectedCountry);
 
-    }, [svgSize, rightDisplay, countryData, selectedCountry]);
+    }, [svgSize, rightDisplay, countryData, selectedCountry, continentFilter]);
 
-    return (
-        <div className="VisContainer">
-            <svg className="SvgBarGraph" ref={svgRef}></svg>  
-            <div className='SideBar'>  
-                <div className='SelectBox' onClick={() => setRightDisplay(0)}>Pick & Choose</div>
-                <div className={`Component SideBarTop ${rightDisplay === 0 ? "display" : "no-display"}`}><SideBarTop/></div>
-                
-                <div className='SelectBox' onClick={() => setRightDisplay(1)}>Country Overview</div>
-                <div className={`Component SideBarMiddle ${rightDisplay === 1 ? "display" : "no-display"}`}><SideBarMiddle selectedCountry={selectedCountry}/></div>  {/* Corrected the condition to `rightDisplay === 1` for `SideBarMiddle` */}
+  return (
+    <div className="VisContainer">
+      <svg className="SvgBarGraph" ref={svgRef}></svg>
+      <div className='SideBar'>
+        <div className='SelectBox' onClick={() => setRightDisplay(0)}>Pick & Choose</div>
+        <div className={`Component SideBarTop ${rightDisplay === 0 ? "display" : "no-display"}`}><SideBarTop setContinentFilter={setContinentFilter}/></div>
 
-                <div className='SelectBox' onClick={() => setRightDisplay(2)}>Consumption Bans</div>
-                <div className={`Component SideBarBottom ${rightDisplay === 2 ? "display" : "no-display"}`}><SideBarBottom setPolicyState={setPolicyState} policyState={policyState}/></div>  {/* Corrected the class to `SideBarBottom` and condition to `rightDisplay === 2` for `SideBarBottom` */}
-            </div>  
-        </div>
-    );
+        <div className='SelectBox' onClick={() => setRightDisplay(1)}>Country Overview</div>
+        <div className={`Component SideBarMiddle ${rightDisplay === 1 ? "display" : "no-display"}`}><SideBarMiddle selectedCountry={selectedCountry}/></div>  {/* Corrected the condition to `rightDisplay === 1` for `SideBarMiddle` */}
+
+        <div className='SelectBox' onClick={() => setRightDisplay(2)}>Consumption Bans</div>
+        <div className={`Component SideBarBottom ${rightDisplay === 2 ? "display" : "no-display"}`}><SideBarBottom setPolicyState={setPolicyState} policyState={policyState}/></div>  {/* Corrected the class to `SideBarBottom` and condition to `rightDisplay === 2` for `SideBarBottom` */}
+      </div>
+    </div>
+  );
 }
+
 export default Vis;
