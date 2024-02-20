0f80bb042470a13002548bd2632dc57b20af09d7
Patrik Lundqvist <ph.lundqvist@gmail.com>
2024-02-20 13:22:04 +0100

    Removed menu bar

--- visapp/src/App.jsx
+++ visapp/src/App.jsx
@@ -1,30 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg' // Ensure these logos are used or remove these imports
-import viteLogo from '/vite.svg'
-import './App.css'
-import MenuBar from './MenuBar';
-import About from './About';
-import HowToUse from './HowToUse';
-import Vis from './Vis';
-import Footer from './Footer';
+import { useState } from "react";
+import reactLogo from "./assets/react.svg"; // Ensure these logos are used or remove these imports
+import viteLogo from "/vite.svg";
+import "./App.css";
+import MenuBar from "./MenuBar";
+import About from "./About";
+import HowToUse from "./HowToUse";
+import Vis from "./Vis";
+import Footer from "./Footer";
 
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
-  return (
-    <Router>
-      <MenuBar />
-      <div className='mainWindow'>
-        <Routes>
-          <Route path="/" element={<Vis />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/howtouse" element={<HowToUse />} />
-        </Routes>
-      </div>
-      <Footer />
-    </Router>
-  )
+	return (
+		<Router>
+			<div className="mainWindow">
+				<Routes>
+					<Route path="/" element={<Vis />} />
+					<Route path="/about" element={<About />} />
+					<Route path="/howtouse" element={<HowToUse />} />
+				</Routes>
+			</div>
+			<Footer />
+		</Router>
+	);
 }
 
 export default App;
