b505be3478de7d6e559bad5816813725256bbc4f
amanda <a.hallstedt@gmail.com>
2024-03-05 10:42:38 +0100

    smol visual changes (unimportant)

--- visapp/src/SideBarTop.jsx
+++ visapp/src/SideBarTop.jsx
@@ -1,12 +1,27 @@
 import { useState } from 'react';
 
-function SideBarTop() {
-  
-  return (
-    <>
-      
-  </>
-  );
+function SideBarTop({ continents, selectedContinents, handleToggleContinent, handleSelectAll, handleDeselectAll }) {
+    return (
+        <div className="sidebar-top-container">
+            <div className="button-group">
+                <button className="select-button" onClick={handleSelectAll}>Select All</button>
+                <button className="select-button" onClick={handleDeselectAll}>Deselect All</button>
+            </div>
+            {continents.map((continent) => (
+                <div key={continent} className="continent-item">
+                    <label>
+                        <input
+                            type="checkbox"
+                            className='checkboxColor'
+                            checked={selectedContinents.includes(continent)}
+                            onChange={() => handleToggleContinent(continent)}
+                        />
+                        <span className="continent-name">{continent}</span>
+                    </label>
+                </div>
+            ))}
+        </div>
+    );
 }
 
 export default SideBarTop;
